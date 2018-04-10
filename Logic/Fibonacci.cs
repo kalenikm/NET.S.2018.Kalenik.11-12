71476e562438bc36aceb68de114b196efab6b067
kalenikm <kalenikmisha@gmail.com>
2018-04-10 18:26:22 +0300

    Init commit

---
+++ Logic/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Logic
+{
+    public static class Fibonacci
+    {
+        public static IEnumerable<BigInteger> GetFibonacciSequence(int count)
+        {
+            BigInteger first = 0;
+            BigInteger second = 1;
+            BigInteger next;
+
+            for (int i = 0; i < count; i++)
+            {
+                next = first + second;
+
+                yield return next;
+
+                first = second;
+                second = next;
+            }
+        }
+    }
+}
